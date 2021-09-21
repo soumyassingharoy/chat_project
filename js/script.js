9db952342caedaf953b6af95ffc8052adbba6ca3
soumya <saurabh@nettrackers.net>
2021-09-21 21:45:05 +0530

    chat massage 2

--- js/script.js
+++ js/script.js
@@ -4,15 +4,19 @@ $(document).ready(function(){
      
   $('.owl-carousel').owlCarousel({
      	loop:true,
-     	margin:10,
-     	nav:true,
-     	autoplay:true,
-     	navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-     	responsive:{
-     		0:{
-     			items:1
-     		}
-     	} 
+      margin:10,
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      }
   });
 
   $(document).ready(function(){
